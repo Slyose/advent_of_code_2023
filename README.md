609cd8e4b46808380ddfb4793c4200aebef2e1a7
Modou Robinson <modour498@gmail.com>
2023-12-04 19:30:15 +0000

    solutions for day 04, both parts

--- README.md
+++ README.md
@@ -11,10 +11,10 @@
 
 <!--SOLUTIONS-->
 
-[![Day](https://badgen.net/badge/01/%E2%98%85%E2%98%85/green)](src/day01)
-[![Day](https://badgen.net/badge/02/%E2%98%85%E2%98%85/green)](src/day02)
+![Day](https://badgen.net/badge/01/%E2%98%86%E2%98%86/gray)
+![Day](https://badgen.net/badge/02/%E2%98%86%E2%98%86/gray)
 [![Day](https://badgen.net/badge/03/%E2%98%85%E2%98%85/green)](src/day03)
-![Day](https://badgen.net/badge/04/%E2%98%86%E2%98%86/gray)
+[![Day](https://badgen.net/badge/04/%E2%98%85%E2%98%85/green)](src/day04)
 ![Day](https://badgen.net/badge/05/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/06/%E2%98%86%E2%98%86/gray)
 ![Day](https://badgen.net/badge/07/%E2%98%86%E2%98%86/gray)
@@ -71,30 +71,30 @@ npm start 1
 
 ```
 Day 01
-Time part 1: 0.391792ms
-Time part 2: 1.520667ms
-Both parts: 1.912459ms
+Time part 1: -
+Time part 2: -
+Both parts: -
 ```
 
 ```
 Day 02
-Time part 1: 0.338416ms
-Time part 2: 0.215708ms
-Both parts: 0.5541240ms
+Time part 1: -
+Time part 2: -
+Both parts: -
 ```
 
 ```
 Day 03
-Time part 1: 14.969ms
-Time part 2: 39.254875ms
-Both parts: 57.876ms
+Time part 1: 13.04ms
+Time part 2: 39.875ms
+Both parts: 52.915ms
 ```
 
 ```
 Day 04
-Time part 1: -
-Time part 2: -
-Both parts: -
+Time part 1: 0.835ms
+Time part 2: 346.283ms
+Both parts: 347.117ms
 ```
 
 ```
@@ -245,8 +245,8 @@ Both parts: -
 ```
 
 ```
-Total stars: 2/50
-Total time: 56.024ms
+Total stars: 8/50
+Total time: 456.032ms
 ```
 
 <!--/RESULTS-->
---
+++ src/day04/index.js
@@ -0,0 +1,141 @@
+import run from "aocrunner"
+
+const parseInput = (rawInput) => rawInput
+
+const part1 = (rawInput) => {
+  const input = parseInput(rawInput)
+
+  const cardsArr = input.split("\n")
+
+  let total = 0
+
+  for (let card of cardsArr) {
+    let splitCard = card.split("|")
+    let winningNumbers = splitCard[0].split(":")
+    let numbersIHave = splitCard[1].match(/[\d]+/g)
+    winningNumbers = winningNumbers[1].match(/[\d]+/g)
+
+    const winningNumbersIHave = []
+
+    for (let winningNumber of winningNumbers) {
+      if (numbersIHave.includes(winningNumber))
+        winningNumbersIHave.push(winningNumber)
+    }
+
+    // variable to store the total points
+    let cardPoints = 0
+
+    let multiplier = 1
+
+    for (let i = 0; i < winningNumbersIHave.length; i++) {
+      const pointsForCurrentCard = multiplier
+
+      // Add the points for the current card to the total
+      cardPoints = multiplier
+
+      // Double the multiplier for subsequent cards
+      multiplier *= 2
+    }
+    total += cardPoints
+  }
+
+  return total
+}
+
+const part2 = (rawInput) => {
+  const input = parseInput(rawInput)
+
+  const originalCards = input.split("\n")
+
+  let liveCards = []
+
+  let totalScratchcards = 0
+
+  for (let card of originalCards) {
+    const cardNumber = card.match(/[\d]+/g)[0]
+
+    let splitCard = card.split("|")
+    let winningNumbers = splitCard[0].split(":")
+    winningNumbers = winningNumbers[1].match(/[\d]+/g)
+    let numbersIHave = splitCard[1].match(/[\d]+/g)
+    liveCards.push({
+      cardNumber,
+      winningNumbers,
+      numbersIHave,
+      quantity: 1,
+    })
+  }
+
+  while (liveCards.length) {
+    let currentCard = liveCards.shift()
+    let { cardNumber, winningNumbers, numbersIHave, quantity } = currentCard
+
+    let winningNumbersIHave = []
+
+    for (let winningNumber of winningNumbers) {
+      if (numbersIHave.includes(winningNumber))
+        winningNumbersIHave.push(winningNumber)
+    }
+
+    let copyNumbers = []
+    let numberOfMatches = winningNumbersIHave.length
+
+    if (numberOfMatches > 0) {
+      // this part figures out the cardnumbers of the cards that should be cloned
+      for (
+        let currentCopyNumber = Number(cardNumber) + 1;
+        Number(currentCopyNumber) <=
+        Number(cardNumber) + Number(numberOfMatches);
+        currentCopyNumber++
+      ) {
+        copyNumbers.push(currentCopyNumber)
+      }
+    }
+
+    for (let copyNumber of copyNumbers) {
+      let copyCard = liveCards.find((card) => {
+        return card.cardNumber == copyNumber
+      })
+      copyCard.quantity += currentCard.quantity
+      //each card has a quantity property of how many there are
+      // i just take the impact that card would have had and do it ${quantity} times
+    }
+
+    totalScratchcards += quantity
+  }
+
+  return totalScratchcards
+}
+
+run({
+  part1: {
+    tests: [
+      {
+        input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+        expected: 13,
+      },
+    ],
+    solution: part1,
+  },
+  part2: {
+    tests: [
+      {
+        input: `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`,
+        expected: 30,
+      },
+    ],
+    solution: part2,
+  },
+  trimTestInputs: true,
+  onlyTests: false,
+})
